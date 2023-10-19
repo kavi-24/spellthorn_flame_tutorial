6fd991c95d7a26d45c7ee5a178182c2d1087d8f3
kavi-24 <srikavipriyan.ma2021@vitstudent.ac.in>
2023-10-19 20:06:45 +0530

    bruh this shii created for desktop... how to add button lol ?!

--- lib/actors/player.dart
+++ lib/actors/player.dart
@@ -1,12 +1,37 @@
 import 'dart:async';
 
 import 'package:flame/components.dart';
+import 'package:flutter/src/services/keyboard_key.g.dart';
+import 'package:flutter/src/services/raw_keyboard.dart';
 import 'package:spellthorn_flame_tutorial/pixel_adventure.dart';
 
+enum PlayerState {
+  idle,
+  running,
+}
+
+enum PlayerDirection {
+  left,
+  right,
+  none,
+}
+
 class Player extends SpriteAnimationGroupComponent
-    with HasGameRef<PixelAdventure> {
-  late final SpriteAnimation idleAnimation;
+    with HasGameRef<PixelAdventure>, KeyboardHandler {
+  String character;
   final double stepTime = 0.05;
+  late final SpriteAnimation idleAnimation;
+  late final SpriteAnimation runningAnimation;
+
+  PlayerDirection playerDirection = PlayerDirection.none;
+  double moveSpeed = 100;
+  Vector2 velocity = Vector2.zero();
+  bool isFacingRight = true;
+
+  Player({
+    position,
+    required this.character,
+  }) : super(position: position);
 
   @override
   FutureOr<void> onLoad() {
@@ -14,14 +39,84 @@ class Player extends SpriteAnimationGroupComponent
     return super.onLoad();
   }
 
+  @override
+  void update(double dt) {
+    _updatePlayerMovement(dt);
+    super.update(dt);
+  }
+
+  @override
+  bool onKeyEvent(RawKeyEvent event, Set<LogicalKeyboardKey> keysPressed) {
+    final isLeftKeyPressed = keysPressed.contains(LogicalKeyboardKey.keyA) ||
+        keysPressed.contains(
+          LogicalKeyboardKey.arrowLeft,
+        );
+    final isRightKeyPressed = keysPressed.contains(LogicalKeyboardKey.keyD) ||
+        keysPressed.contains(
+          LogicalKeyboardKey.arrowRight,
+        );
+
+    if (isLeftKeyPressed && isRightKeyPressed) {
+      playerDirection = PlayerDirection.none;
+    } else if (isLeftKeyPressed) {
+      playerDirection = PlayerDirection.left;
+    } else if (isRightKeyPressed) {
+      playerDirection = PlayerDirection.right;
+    } else {
+      playerDirection = PlayerDirection.right;
+    }
+    return super.onKeyEvent(event, keysPressed);
+  }
+
   void _loadAllAnimations() {
-    idleAnimation = SpriteAnimation.fromFrameData(
-      gameRef.images.fromCache("Main Characters/Ninja Frog/Idle (32x32).png"),
+    idleAnimation = _spriteAnimation("Idle", 11);
+    runningAnimation = _spriteAnimation("Run", 12);
+
+    // list of all animations
+    animations = {
+      PlayerState.idle: idleAnimation,
+      PlayerState.running: runningAnimation,
+    };
+
+    // set current animation
+    current = PlayerState.idle;
+  }
+
+  SpriteAnimation _spriteAnimation(String state, int amount) {
+    return SpriteAnimation.fromFrameData(
+      gameRef.images.fromCache("Main Characters/$character/$state (32x32).png"),
       SpriteAnimationData.sequenced(
-        amount: 11,
+        amount: amount,
         stepTime: stepTime, // 20 fps or 50 ms
         textureSize: Vector2.all(32),
       ),
     );
+  }
+
+  void _updatePlayerMovement(double dt) {
+    double dirX = 0.0;
+    switch (playerDirection) {
+      case PlayerDirection.left:
+        if (isFacingRight) {
+          flipHorizontallyAroundCenter();
+          isFacingRight = false;
+        }
+        current = PlayerState.running;
+        dirX -= moveSpeed;
+        break;
+      case PlayerDirection.right:
+        if (!isFacingRight) {
+          flipHorizontallyAroundCenter();
+          isFacingRight = true;
+        }
+        current = PlayerState.running;
+        dirX += moveSpeed;
+        break;
+      case PlayerDirection.none:
+        current = PlayerState.idle;
+        break;
+    }
+    velocity = Vector2(dirX, 0);
+    position += velocity * dt;
   }
 }
--- lib/pixel_adventure.dart
+++ lib/pixel_adventure.dart
@@ -3,13 +3,16 @@ import 'dart:ui';
 
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
+import 'package:flame/input.dart';
 import 'package:spellthorn_flame_tutorial/levels/level.dart';
 
-class PixelAdventure extends FlameGame {
+class PixelAdventure extends FlameGame with HasKeyboardHandlerComponents {
   @override
   Color backgroundColor() => const Color(0xFF211F30);
   @override
-  final world = Level();
+  final world = Level(
+    levelName: "Level-01"
+  );
   late final CameraComponent cam;
 
   @override
